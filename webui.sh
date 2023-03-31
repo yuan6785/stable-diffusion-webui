fa06b65df9691e518a95c512ffef82f775cdf0df
yuan6785 <yuanxiao6785@gmail.com>
2023-03-31 12:28:54 +0800

    Update webui.sh

--- webui.sh
+++ webui.sh
@@ -149,7 +149,7 @@ else
     printf "Clone stable-diffusion-webui"
     printf "\n%s\n" "${delimiter}"
     "${GIT}" clone https://github.com/yuan6785/stable-diffusion-webui "${clone_dir}"
-    cd "${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
+    cd "${clone_dir}"/ || git checkout 20230222 || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
 fi
 
 printf "\n%s\n" "${delimiter}"
