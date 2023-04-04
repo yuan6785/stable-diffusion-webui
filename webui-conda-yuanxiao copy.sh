d7212f7ce45eb6ed129187e4c8176ebd597ccde2
mac yx <fsdf@f.com>
2023-04-04 11:36:29 +0800

    1

--- webui-conda-yuanxiao copy.sh
+++ webui-conda-yuanxiao copy.sh
@@ -4,60 +4,17 @@
 # change the variables in webui-user.sh instead #
 #################################################
 
-# If run from macOS, load defaults from webui-macos-env.sh
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    if [[ -f webui-macos-env.sh ]]
-        then
-        source ./webui-macos-env.sh
-    fi
-fi
 
-# Read variables from webui-user.sh
-# shellcheck source=/dev/null
-if [[ -f webui-user.sh ]]
-then
-    source ./webui-user.sh
-fi
 
-# Set defaults
-# Install directory without trailing slash
-if [[ -z "${install_dir}" ]]
-then
-    install_dir="/home/$(whoami)"
-fi
+install_dir="/home/$(whoami)"
+clone_dir="stable-diffusion-webui"
+python_cmd="python3"
+export GIT="git"
 
-# Name of the subdirectory (defaults to stable-diffusion-webui)
-if [[ -z "${clone_dir}" ]]
-then
-    clone_dir="stable-diffusion-webui"
-fi
-
-# python3 executable
-if [[ -z "${python_cmd}" ]]
-then
-    python_cmd="python3"
-fi
-
-# git executable
-if [[ -z "${GIT}" ]]
-then
-    export GIT="git"
-fi
-
-# python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-# if [[ -z "${venv_dir}" ]]
-# then
-#     venv_dir="venv"
-# fi
-
-if [[ -z "${LAUNCH_SCRIPT}" ]]
-then
-    LAUNCH_SCRIPT="launch.py"
-fi
+LAUNCH_SCRIPT="launch.py"
 
 # this script cannot be run as root by default
 can_run_as_root=0
-
 # read any command line flags to the webui.sh script
 while getopts "f" flag > /dev/null 2>&1
 do
@@ -132,24 +89,17 @@ do
     fi
 done
 
-if ! "${python_cmd}" -c "import venv" &>/dev/null
-then
-    printf "\n%s\n" "${delimiter}"
-    printf "\e[1m\e[31mERROR: python3-venv is not installed, aborting...\e[0m"
-    printf "\n%s\n" "${delimiter}"
-    exit 1
-fi
-
 cd "${install_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/, aborting...\e[0m" "${install_dir}"; exit 1; }
 if [[ -d "${clone_dir}" ]]
 then
+    printf "hahha------"
     cd "${clone_dir}"/ || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
 else
     printf "\n%s\n" "${delimiter}"
     printf "Clone stable-diffusion-webui"
     printf "\n%s\n" "${delimiter}"
     "${GIT}" clone https://github.com/yuan6785/stable-diffusion-webui "${clone_dir}"
-    cd "${clone_dir}"/ && git checkout b8d905ce84c530d2f3b98e021ddad013e0bbf358 || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
+    cd "${clone_dir}"/ && git checkout 39bf8e1ad4e9ba35f7848bd1e8974292a9460025 || { printf "\e[1m\e[31mERROR: Can't cd to %s/%s/, aborting...\e[0m" "${install_dir}" "${clone_dir}"; exit 1; }
 fi
 
 printf "\n%s\n" "${delimiter}"
